2fefcac55d29a5c82b5682bf0bb4c3825a12a816
NikDychek <153540151+NikDychek@users.noreply.github.com>
2024-03-03 19:53:33 +0300

    Dychak_HW_25

--- app1/urls.py
+++ app1/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create/', views.post_create, name='post_create'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
